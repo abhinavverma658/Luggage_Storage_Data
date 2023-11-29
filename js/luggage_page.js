e694911ac472c5f44ab5133906b5ba7f6e2c97b4
Abhinav Verma <abhinavverma658@gmail.com>
2023-11-30 01:23:31 +0530

    Final Commit

---
+++ js/luggage_page.js
@@ -0,0 +1,14 @@
+var Mainimg = document.getElementById('Main-img');
+var smalling = document.getElementsByClassName('small');
+smalling[0].onclick = function(){
+  Mainimg.src=smalling[0].src;
+}
+smalling[1].onclick = function(){
+  Mainimg.src=smalling[1].src;
+}
+smalling[2].onclick = function(){
+  Mainimg.src=smalling[2].src;
+}
+smalling[3].onclick = function(){
+  Mainimg.src=smalling[3].src;
+}
